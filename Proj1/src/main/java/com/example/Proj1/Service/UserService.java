17a092f7d22c529eb86235f4980013a0d33b55b3
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-05-16 23:44:21 +0530

    feat: hibernate in proj1

---
+++ Proj1/src/main/java/com/example/Proj1/Service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.Proj1.Service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+    public String getUser() {
+        return "Hi";
+    }
+}
---
+++ Proj1/src/main/java/com/example/Proj1/controllers/User.java
@@ -0,0 +1,23 @@
+package com.example.Proj1.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Proj1.Service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class User {
+
+    @Autowired
+    UserService s1;
+
+    @GetMapping("/user")
+    public String getUseString() {
+        return s1.getUser();
+    }
+    
+}
